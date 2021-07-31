35d52a068308414f8f98fc2ab5b972b712d7e171
Advik-Garg <83994800+Advik-Garg@users.noreply.github.com>
2021-07-31 14:46:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var something="My first console project!"
+console.log (something)
+var firstname="Advik "
+var lastname="Garg"
+var fullname= firstname.concat(lastname)
+console.log(fullname)
+var n1=500
+var n2=100
+var n3=50
+var sum=n1+n2+n3
+console.log(sum)
+var subtract=n1-n2-n3
+console.log(subtract)
+var multiply=n1*n2*n3
+console.log(multiply)
+var divide=n1/n2/n3
+console.log(divide)
+console.log(fullname.length)
+var man=Math.max(n1,n2,n3)
+console.log(man)
+var somd=Math.min (n1,n2,n3)
+console.log(somd)
